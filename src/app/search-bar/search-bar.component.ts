e6b5cdcd94ffa25947da537a77c9efbe93f725c0
kchyzhyk <kchyzhykova888@gmail.com>
2024-11-19 13:15:58 +0100

    upd

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,18 +1,54 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { debounceTime, distinctUntilChanged, Subject, switchMap } from 'rxjs';
+import { ApiService } from '../api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit, OnDestroy {
   searchTerm: string = '';
+  searchResults: any[] = [];
+  private searchSubject: Subject<string> = new Subject<string>();
   @Output() search = new EventEmitter<string>();
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.searchSubject
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap((term) => {
+          if (term.length >= 3) {
+            return this.apiService.search(term, 'movie');
+          } else {
+            return [];
+          }
+        })
+      )
+      .subscribe((results) => {
+        this.searchResults = results?.results || [];
+        this.search.emit(results);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubject.unsubscribe();
+  }
+
   onSearch(): void {
-    this.search.emit(this.searchTerm);
+    this.searchSubject.next(this.searchTerm);
   }
 }
